e208360d3e38b0b9e4ff93765f5a4003284cd51d
NMuradyan <nar.muradian@gmail.com>
2020-08-11 15:40:53 +0400

    Fixed

--- JS/Homework5.js
+++ JS/Homework5.js
@@ -2,9 +2,9 @@ window.onload = function changeColor() {
     let fruitArray = document.getElementsByTagName('li');
     for (let i = 0; i < fruitArray.length; i++) {
         if (i%2 == 0) {
-            fruitArray[i].style.backgroundColor = 'red';
+            fruitArray[i].style.color = 'red';
         } else {
-            fruitArray[i].style.backgroundColor = 'green';
+            fruitArray[i].style.color = 'green';
         }
         
     }
